805689febff66918da435e7c320a7633ab713f9b
shoney <ddmczp@naver.com>
2023-05-17 23:49:48 +0900

    CSS tailwind적용

--- src/App.js
+++ src/App.js
@@ -33,12 +33,11 @@ export default function App() {
   };
 
   return (
-    <div className="container">
-      <div className="todoBlock">
-        <div className="title">
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-50">
+      <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4 lg:max-w-lg">
+        <div className="flex justify-between mb-3">
           <h1>TO DO LIST</h1>
         </div>
-
         <List todoData={todoData} setTodoData={setTodoData} />
         <Form handleSubmit={handleSubmit} value={value} setValue={setValue} />
       </div>
